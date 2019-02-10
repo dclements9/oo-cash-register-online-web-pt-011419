467833cbce5cae9b4ce1a9102339e18205db0a9f
Dylan Clements <dclements@protonmail.com>
2019-02-10 20:14:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,12 @@
 class CashRegister
   attr_accessor :total, :discount
+  @@items = []
 
   def initialize(discount = 0)
     @total = 0
     @discount = discount
   end
-  def add_item(title, price)
-    @total += price
+  def add_item(title, price, quantity = 1)
+    @total += (price*quantity)
   end
 end
